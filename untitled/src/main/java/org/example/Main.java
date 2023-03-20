50b1bd1a620ee03982e6727d764ec52eaa514ba9
JoaoESmoreira <jesmoreira2010@gmail.com>
2023-03-20 14:20:56 +0000

    read urls

---
+++ untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String url = "https://pedror.dev";
+
+        try {
+            Document doc = Jsoup.connect(url).get();
+            StringTokenizer tokens = new StringTokenizer(doc.text());
+            int countTokens = 0;
+
+            while (tokens.hasMoreElements() && countTokens++ < 100)
+                System.out.println(tokens.nextToken().toLowerCase());
+
+            Elements links = doc.select("a[href]");
+
+            for (Element link : links)
+                System.out.println(link.text() + "\n" + link.attr("abs:href") + "\n");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
