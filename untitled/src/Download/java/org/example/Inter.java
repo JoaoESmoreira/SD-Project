b3180fee5bd0c25913fa8a57685fcc514185f28d
Tomás Pinto <tomascerveira10@gmail.com>
2023-03-22 14:08:54 +0000

    wip

--- untitled/src/Download/java/org/example/Inter.java
+++ untitled/src/Download/java/org/example/Inter.java
@@ -3,5 +3,6 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Inter extends Remote {
-	public String sayURL(String s) throws java.rmi.RemoteException;
+	public String sayURL(String s) throws RemoteException;
+	public String saySearch(String s) throws RemoteException;
 }
