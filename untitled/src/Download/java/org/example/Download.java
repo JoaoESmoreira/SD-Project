e7ab55c94f0fd5a243d706d14b9a074b06767644
Tomás Pinto <tomascerveira10@gmail.com>
2023-03-23 15:47:48 +0000

    wip

--- untitled/src/Download/java/org/example/Download.java
+++
@@ -1,5 +0,0 @@
-import java.rmi.*;
-
-public interface Download extends Remote {
-    String debug() throws java.rmi.RemoteException;
-}
--- untitled/src/Download/java/org/example/SearchModule.java
+++ untitled/src/Download/java/org/example/SearchModule.java
@@ -1,3 +1,4 @@
+import java.net.MalformedURLException;
 import java.rmi.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -6,6 +7,8 @@ import java.rmi.server.*;
 
 public class SearchModule extends UnicastRemoteObject implements Inter{
 
+	private Binterface c;
+
     public SearchModule() throws RemoteException {
 		super();
 	}
@@ -13,7 +16,30 @@ public class SearchModule extends UnicastRemoteObject implements Inter{
     public String sayURL(String s) throws RemoteException {
         System.out.println("server: "+s);
 
-		return "URL Sent";
+		
+	 	//System.setSecurityManager(new RMISecurityManager()); 
+		try{
+			Binterface h2 = (Binterface) LocateRegistry.getRegistry(9000).lookup("barrel");
+
+			return h2.Infos(s);
+	
+		}catch(Exception e){
+			System.out.println("Exception in sayURL: " + e);
+			e.printStackTrace();
+		}
+			
+		
+		//try {
+		//	Binterface c = (Binterface) Naming.lookup("barrel");
+		//	return 
+		//}
+		//catch (Exception e) {
+		//	System.out.println("Exception in comunication: " + e);
+		//	e.printStackTrace();
+		//}
+		
+
+		return "Error!";
     }
 
 	public String saySearch(String s) throws RemoteException {
@@ -22,17 +48,37 @@ public class SearchModule extends UnicastRemoteObject implements Inter{
 		return "Search Sent";
 	}
 
+	public void setC(Binterface c){
+		this.c = c;
+	}
+
+	public Binterface getC(){
+		return c;
+	}
+
     public static void main(String[] args) {
         try {
 
-			//criar thread para comunicar com barrel
+			System.getProperties().put("java.security.policy","policy.all") ;
+			//System.setSecurityManager(new RMISecurityManager()); 
 
-			//isto fica no programa principal
+			
+
 			Inter h = new SearchModule();
 		    LocateRegistry.createRegistry(7000).rebind("ALGO", h);
+
+			//SearchModule h2 = new SearchModule();
+			//h2.setC((Binterface) Naming.lookup("barrel"));
+
 			System.out.println("Search Module ready.");
+
 		} catch (RemoteException re) {
 			System.out.println("Exception in main: " + re);
-		}
+		} 
+		//catch (MalformedURLException me){
+		//	System.out.println("Exception : " + me);
+		//} catch (NotBoundException ne){
+		//	System.out.println("in main: " + ne);
+		//}
     }
 }
